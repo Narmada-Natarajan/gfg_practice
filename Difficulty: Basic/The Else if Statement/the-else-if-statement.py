209c01c720f53465e0ca2ed1d101f83f27edfd17
Narmada Natarajan <narmadanatarajan3629@gmail.com>
2026-08-01 11:45:36 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/The Else if Statement/the-else-if-statement.py
@@ -0,0 +1,10 @@
+class Solution:
+    def utility(self, number):
+        
+        if number>100:
+            print("Big")
+            
+        elif number<10:
+            print("Small")
+        else:
+            print("Number")
