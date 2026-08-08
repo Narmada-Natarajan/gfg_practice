2eeada73d00eb4eb0f6800927e481a2deedce0e4
Narmada Natarajan <narmadanatarajan3629@gmail.com>
2026-08-08 18:30:11 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Count Smaller in an Array/count-smaller-in-an-array.py
@@ -0,0 +1,10 @@
+class Solution:
+    def countOfElements(self, x, arr):
+        
+        cnt=0
+        
+        for i in arr:
+            if i<=x:
+                cnt+=1
+        return cnt
+
