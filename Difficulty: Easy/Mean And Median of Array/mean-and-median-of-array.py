b5a526dbe15f0159c2053342f18a1d59e7630887
Narmada Natarajan <narmadanatarajan3629@gmail.com>
2026-08-10 23:15:07 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Mean And Median of Array/mean-and-median-of-array.py
@@ -0,0 +1,12 @@
+import statistics as s
+import math
+
+class Solution:
+    def median(self, arr):
+        # code here
+        return math.floor(s.median(arr))
+        
+    
+    def mean(self, arr):
+        # code here
+        return math.floor(s.mean(arr))
