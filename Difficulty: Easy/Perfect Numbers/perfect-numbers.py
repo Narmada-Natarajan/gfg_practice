35dba0fca32470268d723ba7b932e08cacad8460
Narmada Natarajan <narmadanatarajan3629@gmail.com>
2026-08-11 20:50:05 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Perfect Numbers/perfect-numbers.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isPerfect(self, n):
+        if n <= 1:
+            return False
+
+        total = 1
+
+        i = 2
+        while i * i <= n:
+            if n % i == 0:
+                total += i
+
+                if i != n // i:
+                    total += n // i
+
+            i += 1
+
+        return total == n
