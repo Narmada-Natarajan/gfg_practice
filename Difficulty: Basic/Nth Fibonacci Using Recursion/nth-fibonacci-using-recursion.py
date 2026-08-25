527e9621897d296202b4f5cd0a7b3012849f78dc
Narmada Natarajan <narmadanatarajan3629@gmail.com>
2026-08-25 14:55:14 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Nth Fibonacci Using Recursion/nth-fibonacci-using-recursion.py
@@ -0,0 +1,12 @@
+class Solution:
+    def nthFibonacci(self, n):
+        
+        a=0
+        b=1
+        
+        for i in range(n):
+            c=a+b
+            a=b
+            b=c
+        return a
+        
