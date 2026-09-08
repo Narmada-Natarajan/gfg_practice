56d016de4c37337585a8efeabed9d02e3662b6b1
Narmada Natarajan <narmadanatarajan3629@gmail.com>
2026-09-08 10:08:03 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Inorder Traversal/inorder-traversal.py
@@ -0,0 +1,27 @@
+''' Structure of Binary Tree Node
+class Node:
+    def __init__(self, val):
+        self.data = val
+        self.left = None
+        self.right = None
+'''
+
+class Solution:
+    def inOrder(self, root):
+        if root==None:
+            return []
+        
+        ans=[]
+        def inorder(node):
+            if node is None:
+                return
+
+            inorder(node.left)
+            ans.append(node.data)
+            inorder(node.right)
+
+        inorder(root)
+
+        return ans
+        
+        
