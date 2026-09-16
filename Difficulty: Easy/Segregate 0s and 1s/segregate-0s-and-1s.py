aa8a52fb226cabaf8b98d2c92e1799df096cc84a
Narmada Natarajan <narmadanatarajan3629@gmail.com>
2026-09-16 14:16:45 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Segregate 0s and 1s/segregate-0s-and-1s.py
+++ Difficulty: Easy/Segregate 0s and 1s/segregate-0s-and-1s.py
@@ -4,19 +4,15 @@ class Solution:
         l=0
         r=len(arr)-1
         
-        while l<r:
+        while l<=r:
             
-            if arr[l]==0:
-                l+=1
-            
-            elif arr[r]==1:
+            if arr[l]==1:
+                arr[l],arr[r]=arr[r],arr[l] #swap
                 r-=1
                 
             else:
-                arr[l],arr[r]=arr[r],arr[l]
                 l+=1
-                r-=1
-                
-        return arr
                 
         
+                
+                
