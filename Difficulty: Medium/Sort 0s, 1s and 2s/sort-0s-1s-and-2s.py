e5142b92d6895b354d5fb037470b416c120584ca
Narmada Natarajan <narmadanatarajan3629@gmail.com>
2026-09-16 14:06:53 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Sort 0s, 1s and 2s/sort-0s-1s-and-2s.py
+++ Difficulty: Medium/Sort 0s, 1s and 2s/sort-0s-1s-and-2s.py
@@ -1,36 +1,26 @@
 class Solution:
     def sort012(self, arr):
-        # code here
-        cnt0=0
-        cnt1=0
-        cnt2=0
         
-        for i in arr:
-            
-            if i==0:
-                cnt0+=1
-            elif i==1:
-                cnt1+=1
-            elif i==2:
-                cnt2+=1
-            
-        i=0
-        while(cnt0>0):
-            arr[i]=0
-            i+=1
-            cnt0-=1
-            
-        while (cnt1>0):
-            arr[i]=1
-            i+=1
-            cnt1-=1
-            
-        while (cnt2>0):
-            arr[i]=2
-            i+=1
-            cnt2-=1
-        return arr
+        s=0
+        m=0
+        e=len(arr)-1
         
+        while m<=e:
             
+            element=arr[m]
             
+            if element==0:
+                arr[m],arr[s]=arr[s],arr[m]
+                s+=1
+                m+=1
+                
+            elif element==1:
+                m+=1
+                
+            else: 
+                arr[m],arr[e]=arr[e],arr[m]
+                e-=1
+                
             
+                
+        
